4b9c6964aa0606e0cf1e4051dcf82d789b342993
blujai831 <webmaster@blujai831.dev>
2024-11-03 22:24:45 -0800

    Fix flickering screen overlay

--- objects/screen_overlay.gd
+++ objects/screen_overlay.gd
@@ -4,7 +4,13 @@ extends Node2D
 @export var color: Color
 
 func _draw():
-	draw_rect(get_viewport_rect()*get_canvas_transform(), color)
+	var camera_rect := get_viewport_rect()*get_canvas_transform()
+	draw_rect(Rect2(
+		camera_rect.position.x - 64,
+		camera_rect.position.y - 64,
+		camera_rect.size.x + 128,
+		camera_rect.size.y + 128
+	), color)
 
 func _process(_delta):
 	queue_redraw()
