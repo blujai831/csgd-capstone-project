57805066cbfaa5dad4130a255f908dd25fcf6d56
blujai831 <webmaster@blujai831.dev>
2024-11-03 22:13:08 -0800

    Fix HintESC incorrectly calculating camera rect.

--- objects/hints/hint_esc.gd
+++ objects/hints/hint_esc.gd
@@ -4,7 +4,7 @@ extends Hint
 @onready var _player_character := $'../../PlayerCharacter' as PlayerCharacter
 
 func _should_open():
-	var rect = get_viewport().get_visible_rect()
+	var rect = get_viewport_rect()*_player_character.get_canvas_transform()
 	return (
 		_player_character.global_position.y >=
 		rect.position.y + rect.size.y + 200
